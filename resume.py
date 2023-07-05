8c5d786e2b11c4910575d55d290ea43217d7cba7
Dishani Sen <42896082+dishanisen@users.noreply.github.com>
2023-07-05 16:14:11 -0400

    Update resume.py

--- resume.py
+++ resume.py
@@ -1,6 +1,6 @@
+pip install streamlit requests streamlit-lottie
 import streamlit as st
 import requests
-import streamlit_lottie
 from streamlit_lottie import st_lottie
 
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
