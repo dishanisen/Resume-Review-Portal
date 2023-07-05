59b49bf0c29b504843c310bd50da5ed7c90a042d
Dishani Sen <42896082+dishanisen@users.noreply.github.com>
2023-07-05 16:18:08 -0400

    Update resume.py

--- resume.py
+++ resume.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-from streamlit-lottie import st_lottie
+from streamlit_lottie import st_lottie
 
 
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
